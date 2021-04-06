ab447742d2dc81c864cbf9d5790a4908549b7c46
madMax342 <maximgel@post.bgu.ac.il>
2021-04-06 18:55:00 +0300

    Update q4.ts

--- src/q4.ts
+++ src/q4.ts
@@ -1,4 +1,4 @@
-import { CExp, parseL3Exp, Exp, isAppExp, isBoolExp, isDefineExp, isIfExp, isLetExp, isNumExp, isPrimOp, isProcExp, isProgram, isStrExp, isVarRef, ProcExp, Program, VarDecl, DefineExp, isAtomicExp } from '../imp/L3-ast';
+import { CExp, parseL3Exp, Exp, isAppExp, isBoolExp, isDefineExp, isIfExp, isLetExp, isNumExp, isPrimOp, isProcExp, isProgram, isStrExp, isVarRef, ProcExp, Program, VarDecl, DefineExp, isAtomicExp, AppExp } from '../imp/L3-ast';
 import { closureToString, compoundSExpToString, isClosure, isCompoundSExp, isEmptySExp, isSymbolSExp, Value } from '../imp/L3-value';
 import { bind, Result, makeFailure, makeOk } from '../shared/result';
 import { isNumber } from '../shared/type-predicates';
@@ -24,7 +24,7 @@ export const unparsePrimOp = (op: CExp, exps: Exp[]): string => {
     //console.log(op);
     if (isPrimOp(op)) {
         if (["+", "-", "/", "*", "<", ">" , "=", "eq?"].includes(op.op)) {
-            return map(unparse, exps).join(` ${op.op === "=" || "eq?" ? `==` : op.op} `);
+            return `(${map(unparse, exps).join(` ${op.op === ("=" || "eq?") ? `==` : op.op} `)})`;
         }
         return  op.op === "number?" ? `lambda ${map(unparse, exps)} : (type(${map(unparse, exps)}) == number)` :
                 op.op === "boolean?" ? `lambda ${map(unparse, exps)} : (type(${map(unparse, exps)}) == bool)` : 
@@ -36,6 +36,11 @@ export const unparsePrimOp = (op: CExp, exps: Exp[]): string => {
     return "nnn";
 
 }
+
+export const unparseAppExp = (ae: AppExp): string =>
+    isPrimOp(ae.rator) ? unparsePrimOp(ae.rator, ae.rands) :
+    isProcExp(ae.rator) ? `${unparseProcExp(ae.rator)}(${unparseLExps(ae.rands)})`:
+    "never";
 
 const unparseProcExp = (pe: ProcExp): string => 
     `(lambda ${map((p: VarDecl) => p.var, pe.args).join(",")} : ${unparseLExps(pe.body)})`
@@ -62,11 +67,11 @@ export const unparse = (exp: Exp | Program): string =>
     isProcExp(exp) ?  unparseProcExp(exp) :
     isIfExp(exp) ? `(${unparse(exp.then)} if ${unparse(exp.test)} else ${unparse(exp.alt)})` :
     //isAppExp(exp) ?  `(${unparse(exp.rator)} ${unparseLExps(exp.rands)})`:
-    isAppExp(exp) ? `(${unparsePrimOp(exp.rator, exp.rands)})` :
+    isAppExp(exp) ? `${unparseAppExp(exp)}` :
     isPrimOp(exp) ?  `${exp.op}` :
     isDefineExp(exp) ?  unparseDefineExp(exp) : //`(define ${exp.var.var} ${unparse(exp.val)})`
     isProgram(exp) ?  `(L3 ${unparseLExps(exp.exps)})` :
     "never";
 
 //self testing
-console.log(bind(bind(p(`(eq? 5 3)`),parseL3Exp),l2ToPython))
+console.log(bind(bind(p(`((lambda (x y) (* x y)) 3 4)`),parseL3Exp),l2ToPython))
