330d9f535f31d281008927182894d56741988df8
madMax342 <maximgel@post.bgu.ac.il>
2021-04-06 17:23:01 +0300

    Update q4.ts

--- src/q4.ts
+++ src/q4.ts
@@ -1,14 +1,14 @@
 import { Exp, isAppExp, isBoolExp, isDefineExp, isIfExp, isLetExp, isNumExp, isPrimOp, isProcExp, isProgram, isStrExp, isVarRef, ProcExp, Program, VarDecl } from '../imp/L3-ast';
 import { closureToString, compoundSExpToString, isClosure, isCompoundSExp, isEmptySExp, isSymbolSExp, Value } from '../imp/L3-value';
 import { Result, makeFailure, makeOk } from '../shared/result';
-import * as pred from '../shared/type-predicates';
+import { isNumber } from '../shared/type-predicates';
 import { map } from "ramda";
 
 const unparseLExps = (les: Exp[]): string =>
     map(l2ToPython, les).join(" ");
 
 export const valueToString = (val: Value): string =>
-    pred.isNumber(val) ?  val.toString() :
+    isNumber(val) ?  val.toString() :
     val === true ? 'true' :
     val === false ? 'false' :
     //T.isString(val) ? `"${val}"` :
@@ -28,15 +28,19 @@ Signature: l2ToPython(l2AST)
 Type: [EXP | Program] => Result<string>
 */
 export const l2ToPython = (exp: Exp | Program): Result<string>  => 
-    isBoolExp(exp) ? makeOk(`${valueToString(exp.val)}`) :
-    isNumExp(exp) ?  makeOk(`${valueToString(exp.val)}`) :
+    makeOk(unparse(exp));
+
+
+
+export const unparse = (exp: Exp | Program): string =>
+    isBoolExp(exp) ? `${valueToString(exp.val)}` :
+    isNumExp(exp) ?  `${valueToString(exp.val)}` :
     //isStrExp(exp) ?  makeOk(valueToString(exp.val)) :
-    isVarRef(exp) ? makeOk(`${exp.var}`) :
-    isProcExp(exp) ?  makeOk(unparseProcExp(exp)) :
-    isIfExp(exp) ?  makeOk(`(if ${l2ToPython(exp.test)} ${l2ToPython(exp.then)} ${l2ToPython(exp.alt)})`) :
-    isAppExp(exp) ?  makeOk(`(${l2ToPython(exp.rator)} ${unparseLExps(exp.rands)})`) :
-    isPrimOp(exp) ?  makeOk(`${exp.op}`) :
-    isDefineExp(exp) ?  makeOk(`(define ${exp.var.var} ${l2ToPython(exp.val)})`) :
-    isProgram(exp) ?  makeOk(`(L3 ${unparseLExps(exp.exps)})`) :
-    makeFailure("never");
-    //makeFailure("TODO")
+    isVarRef(exp) ? `${exp.var}` :
+    isProcExp(exp) ?  unparseProcExp(exp) :
+    isIfExp(exp) ? `(${l2ToPython(exp.then)} if ${l2ToPython(exp.test)} else ${l2ToPython(exp.alt)})` :
+    isAppExp(exp) ?  `(${l2ToPython(exp.rator)} ${unparseLExps(exp.rands)})`:
+    isPrimOp(exp) ?  `${exp.op}` :
+    isDefineExp(exp) ?  `(define ${exp.var.var} ${l2ToPython(exp.val)})` :
+    isProgram(exp) ?  `(L3 ${unparseLExps(exp.exps)})` :
+    "never";
